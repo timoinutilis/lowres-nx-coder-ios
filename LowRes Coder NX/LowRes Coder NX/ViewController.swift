2e9d75a96a90a1ffc4e5e90811ba2fd55322817b
Timo Kloss <timo@inutilis.com>
2017-09-21 21:13:24 +0200

    Created Xcode project.

---
+++ LowRes Coder NX/LowRes Coder NX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LowRes Coder NX
+//
+//  Created by Timo Kloss on 21/9/17.
+//  Copyright © 2017 Inutilis Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
